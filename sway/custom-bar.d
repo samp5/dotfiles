13d2abbd86b107b270a6f0a08e647874b2081526
Sam Praneis <sampraneis@gmail.com>
2024-04-10 12:37:49 -0500

    bluetooth rofi, media and system modes, cpu temp

---
+++ sway/custom-bar.d
@@ -0,0 +1,18 @@
+bar {
+    position top
+    # When the status_command prints a new line to stdout, swaybar updates.
+    # The default just shows the current date and time.
+    status_command while ~/dotfiles/sway/scripts/sway_bar.sh; do sleep 1; done
+    
+    
+
+    colors {
+        background #1F1F28
+
+        #                 border background text
+        inactive_workspace #1F1F28 #32323200 #1F1F28
+        focused_workspace #1F1F28 #7E9CD8 #1F1F28
+        urgent_workspace  #7E9CD8 #1F1F28 #DCD7BA
+
+    }
+}
