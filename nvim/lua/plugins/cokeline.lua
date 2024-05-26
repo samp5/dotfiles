9752a8b72f8e79ca40c4e5c0dca52ece68e20f28
Sam Praneis <sampraneis@gmail.com>
2024-05-26 18:40:22 -0500

    Tab section with bad on-click

--- nvim/lua/plugins/cokeline.lua
+++ nvim/lua/plugins/cokeline.lua
@@ -3,19 +3,16 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
+    'nvim-lua/plenary.nvim',
   },
 
   config = function()
+    local popup = require("plenary.popup")
+
+    local function create_window(tab)
+    end
+
     require('cokeline').setup({
-      sidebar = {
-        filetype = 'NvimTree',
-        components = {
-          {
-            text = '  NvimTree',
-            style = 'bold',
-          },
-        }
-      },
       components = {
         {
           text = function(buffer) return ' ' .. buffer.index .. ' ' end,
@@ -40,7 +37,6 @@ return {
         },
         {
           text = function(buffer) if buffer.diagnostics.errors > 0 then return '   ' else return '' end end,
-
           -- same red as return statements
           fg = '#f45a60'
         },
@@ -57,7 +53,85 @@ return {
           text = ' ',
         }
       },
+      tabs = {
+        placement = "right",
+        components = {
+          {
+            text = function(tab)
+              if tab.is_active then
+                return ' ' .. tab.index .. ' '
+              else
+                return ' ' .. tab.index .. ' '
+              end
+            end,
+            fg = function(tab) if tab.is_active then return "#DCD7BA" else return "#938AA9" end end,
+            bg = function(tab) if tab.is_active then return "#2A2A37" else return "#16161D" end end,
+            bold = true,
+            on_click = function(_, _, _, _, tab)
+              local files = {}
+              local n = 0
+              local windows = vim.api.nvim_tabpage_list_wins(tab.number)
+              for i, w in ipairs(windows) do
+                local buf = vim.api.nvim_win_get_buf(w)
+                local path = vim.api.nvim_buf_get_name(buf)
+                n = n + 1
+                files[i] = path
+              end
+
+              local width       = 60
+              local height      = 10
+              local bufner      = vim.api.nvim_create_buf(false, true)
+              local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+              local win_id, win = popup.create(bufner, {
+                title = "Windows",
+                line = math.floor(((vim.o.lines - height) / 2) - 1),
+                col = math.floor((vim.o.columns - width) / 2),
+                minwidth = width,
+                minheight = height,
+                borderchars = borderchars,
+              })
+              vim.api.nvim_buf_set_lines(bufner, 0, n, false, files)
+              local command = ":lua vim.api.nvim_win_close(" .. win_id .. ",true)<CR>"
+              vim.api.nvim_buf_set_keymap(bufner, "n", "q", command, { silent = true })
+            end,
+          },
+        }
+      }
     })
+
+    vim.keymap.set('n', '<leader>tv', function()
+      local files = {}
+      local n = 0
+      local tab = vim.api.nvim_get_current_tabpage()
+      local windows = vim.api.nvim_tabpage_list_wins(tab)
+      for i, w in ipairs(windows) do
+        local buf = vim.api.nvim_win_get_buf(w)
+        local path = vim.api.nvim_buf_get_name(buf)
+        n = n + 1
+        files[i] = path
+      end
+
+      local width       = 60
+      local height      = 10
+      local bufner      = vim.api.nvim_create_buf(false, true)
+      local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+      local win_id, win = popup.create(bufner, {
+        title = "Windows",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+      })
+      vim.api.nvim_buf_set_lines(bufner, 0, n, false, files)
+      local command = ":lua vim.api.nvim_win_close(" .. win_id .. ",true)<CR>"
+      vim.api.nvim_buf_set_keymap(bufner, "n", "q", command, { silent = true })
+      local file_path = vim.api.nvim_get_current_line()
+      local command2 = ":q | :e " .. file_path .. "<CR>"
+      vim.api.nvim_buf_set_keymap(bufner, "n", "<CR>", command2, { silent = true })
+    end)
 
     vim.keymap.set('n', 'H', '<Plug>(cokeline-focus-prev)', { noremap = true, desc = 'Focus previous buffer' })
     vim.keymap.set('n', 'L', '<Plug>(cokeline-focus-next)', { noremap = true, desc = 'Focus next buffer' })
