3b5e55597b96fbe5509a87a1e2160cb74bbf8be4
Sam Praneis <sampraneis@gmail.com>
2024-06-09 19:30:49 -0500

    .

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -92,6 +92,29 @@ return {
         vim.fn.jobstart({ "xdg-open", url })
       end,
 
+      note_frontmatter_func = function(note)
+        -- Add the title of the note as an alias.
+        if note.title then
+          note:add_alias(note.title)
+        end
+
+        --@type string[];
+        local topics = {}
+
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags, topics = topics }
+
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
+
+
       ui = {
         hl_groups = {
           ObsidianTodo = { bold = true, fg = p.springViolet1 },
